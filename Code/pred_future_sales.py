eb135ded613c95c6ba4c8efdf7254302f95ac4f1
maskarasaket <maskarasaket@gmail.com>
2020-04-25 16:07:21 +0530

    fixed bug in logs

--- Code/pred_future_sales.py
+++ Code/pred_future_sales.py
@@ -126,13 +126,13 @@ if 'item_price' not in ignorecols:
     ### First do forward fill, then median by mkey, then overall median
     print("Treating missing price")
     rawfeatures.groupby(['shop_id', 'item_id'])['item_price'].fillna(method='ffill', inplace=True)
-    print(f"Missing count after ffill : {rawfeatures.item_price.isna().sum}")
+    print(f"Missing count after ffill : {rawfeatures.item_price.isna().sum()}")
     
     rawfeatures['item_price'] = rawfeatures.groupby(['shop_id', 'item_id']).transform(lambda x : x.fillna(x.median()))
-    print(f"Missing count after group wise median : {rawfeatures.item_price.isna().sum}")
+    print(f"Missing count after group wise median : {rawfeatures.item_price.isna().sum()}")
     
     rawfeatures['item_price'] = rawfeatures['itemprice'].fillna(rawfeatures['itemprice'].median())
-    print(f"Missing count after overall median : {rawfeatures.item_price.isna().sum}")
+    print(f"Missing count after overall median : {rawfeatures.item_price.isna().sum()}")
 
     # res = Parallel(n_jobs=-1, verbose=5)(delayed(ffillparallel)(group) for _, group in rawfeatures.groupby(['shop_id', 'item_id']))
     # rawfeatures = pd.concat(res)
